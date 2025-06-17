810a90c954d68543c24ba660343d37a801f2b171
3illBaby <thrillanon@outlook.com>
2025-06-17 21:50:48 +0100

    Add medical record management tests and functionality
    The changes add several test cases around medical records, patient and
    pharmacist management, including access control and record creation
    workflows.

--- test/AllofHealthV2.test.sol
+++ test/AllofHealthV2.test.sol
@@ -8,6 +8,11 @@ import {AllofHealthv2} from "../src/AllofHealthV2.sol";
 contract AllofHealthV2Test is Test {
     event HospitalCreated(address indexed admin, uint256 indexed hospitalId);
     event SystemAdminAdded(address indexed admin, uint256 indexed adminId);
+    event MedicalRecordAccessApproved(
+        address indexed patient,
+        address indexed approvedDoctor,
+        uint256 indexed medicalRecordId
+    );
     Deployer deployer;
     AllofHealthv2 allofHealth;
 
@@ -17,6 +22,7 @@ contract AllofHealthV2Test is Test {
     address public deployerAddress = makeAddr("user5");
     address public hospitalAdminAddress = makeAddr("user3");
     address public adminAddress = makeAddr("user4");
+    address public pharmacistAddress = makeAddr("user6");
 
     function setUp() external {
         deployer = new Deployer();
@@ -25,6 +31,44 @@ contract AllofHealthV2Test is Test {
         vm.deal(doctorAddress, STARTING_BALANCE);
         vm.deal(patientAddress, STARTING_BALANCE);
         vm.deal(adminAddress, STARTING_BALANCE);
+        vm.deal(pharmacistAddress, STARTING_BALANCE);
+    }
+
+    modifier bootstrap_hospital() {
+        vm.startPrank(hospitalAdminAddress);
+        allofHealth.createHospital();
+        vm.stopPrank();
+        _;
+    }
+
+    modifier bootstrap_doctor() {
+        vm.startPrank(doctorAddress);
+        allofHealth.createDoctor(1, doctorAddress);
+        vm.stopPrank();
+        _;
+    }
+
+    modifier bootstrap_pharmacist() {
+        vm.startPrank(pharmacistAddress);
+        allofHealth.createPharmacist(1, pharmacistAddress);
+        vm.stopPrank();
+        _;
+    }
+
+    modifier bootstrap_patient() {
+        vm.startPrank(patientAddress);
+        allofHealth.addPatient();
+        vm.stopPrank();
+        _;
+    }
+
+    modifier bootstrap_approveDoctor() {
+        vm.startPrank(hospitalAdminAddress);
+        uint256 hospitalId = 1;
+        uint256 doctorId = 1;
+        allofHealth.approveDoctor(doctorAddress, hospitalId, doctorId);
+        vm.stopPrank();
+        _;
     }
 
     function testCreateHospitalSuccessfully() external {
@@ -66,5 +110,126 @@ contract AllofHealthV2Test is Test {
         uint256 adminCount = allofHealth.systemAdminCount();
 
         assertEq(adminCount, 2);
+    }
+
+    function test_createPatient() external {
+        vm.startPrank(patientAddress);
+        allofHealth.addPatient();
+        vm.stopPrank();
+
+        uint256 patientCount = allofHealth.patientCount();
+
+        assertEq(patientCount, 1);
+    }
+
+    function test_createPharmacist() external bootstrap_hospital {
+        vm.startPrank(pharmacistAddress);
+        allofHealth.createPharmacist(1, pharmacistAddress);
+        vm.stopPrank();
+
+        uint256 hospitalCount = allofHealth.hospitalCount();
+        uint256 pharmacistCount = allofHealth.pharmacistCount();
+
+        assertEq(hospitalCount, 1);
+        assertEq(pharmacistCount, 1);
+    }
+
+    function test_approveDoctor() external bootstrap_hospital bootstrap_doctor {
+        vm.startPrank(hospitalAdminAddress);
+        uint256 hospitalId = 1;
+        uint256 doctorId = 1;
+        allofHealth.approveDoctor(doctorAddress, hospitalId, doctorId);
+        vm.stopPrank();
+
+        bool isDoctorApproved = allofHealth.approvedDoctors(doctorAddress);
+        assertEq(isDoctorApproved, true);
+    }
+
+    function test_approvePharmacist()
+        external
+        bootstrap_hospital
+        bootstrap_pharmacist
+    {
+        vm.startPrank(hospitalAdminAddress);
+        uint256 hospitalId = 1;
+        uint256 pharmacistId = 1;
+        allofHealth.approvePharmacist(
+            pharmacistAddress,
+            hospitalId,
+            pharmacistId
+        );
+        vm.stopPrank();
+
+        bool isPharmacistApproved = allofHealth.approvedPharmacists(
+            pharmacistAddress
+        );
+        assertEq(isPharmacistApproved, true);
+    }
+
+    function test_approveAccessToAddNewRecord()
+        external
+        bootstrap_hospital
+        bootstrap_doctor
+        bootstrap_approveDoctor
+        bootstrap_patient
+    {
+        uint256 patientId = 1;
+        vm.startPrank(patientAddress);
+        allofHealth.approveAccessToAddNewRecord(doctorAddress, patientId);
+        vm.stopPrank();
+
+        bool isAccessApproved = allofHealth.isApprovedByPatientToAddNewRecord(
+            patientId,
+            doctorAddress
+        );
+        assertEq(isAccessApproved, true);
+    }
+
+    function initApproveAccessToAddNewRecord()
+        internal
+        bootstrap_hospital
+        bootstrap_doctor
+        bootstrap_approveDoctor
+        bootstrap_patient
+    {
+        uint256 patientId = 1;
+        vm.startPrank(patientAddress);
+        allofHealth.approveAccessToAddNewRecord(doctorAddress, patientId);
+        vm.stopPrank();
+    }
+
+    function test_addMedicalRecord() external {
+        initApproveAccessToAddNewRecord();
+        uint256 patientId = 1;
+        string memory recordDetailsUri = "https://example.com/medical-record";
+
+        vm.startPrank(doctorAddress);
+        allofHealth.addMedicalRecord(
+            doctorAddress,
+            patientAddress,
+            patientId,
+            recordDetailsUri
+        );
+        vm.stopPrank();
+
+        uint256 medicalRecordId = 1;
+
+        vm.startPrank(patientAddress);
+        string memory recordUri = allofHealth.viewMedicalRecord(
+            medicalRecordId,
+            patientId,
+            patientAddress
+        );
+        vm.stopPrank();
+
+        console.log("Expected:", recordDetailsUri);
+        console.log("Actual:", recordUri);
+        bool isDoctorStillApprovedToAddRecord = allofHealth
+            .isApprovedByPatientToAddNewRecord(patientId, doctorAddress);
+        assertEq(isDoctorStillApprovedToAddRecord, false);
+        assertEq(
+            keccak256(abi.encodePacked(recordUri)),
+            keccak256(abi.encodePacked(recordDetailsUri))
+        );
     }
 }
